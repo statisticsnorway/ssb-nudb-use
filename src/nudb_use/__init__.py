8243cfb786adf52eb6125c6d1033cb13f55d81ad
kss2k <slupphaugkjell@gmail.com>
2025-12-12 11:07:44 +0100

    Add `__version__` attribute

--- src/nudb_use/__init__.py
+++ src/nudb_use/__init__.py
@@ -1,5 +1,7 @@
 """NUDB Use - is a usage-package for the Norwegian National Education Database cloud-data. Both for data-consumers and data-deliverers. Requires access to NUDBs shared data in most instances."""
 
+import importlib.metadata
+
 from nudb_config import settings
 
 from nudb_use.metadata import find_var
@@ -32,4 +34,15 @@ __all__ = [
 try:
     _check_ssb_nudb_config_version()
 except Exception as err:
-    logger.warning(f"Unable to validate `ssb-nudb-config` version, message:\n{err}")
+    logger.warning(f"Unable to validate `ssb-nudb-config` version!\nMessage: {err}")
+
+
+try:
+    try:
+        __version__ = importlib.metadata.version(__name__)
+    except Exception:
+        __version__ = importlib.metadata.version("ssb-nudb-use")
+
+except Exception as err:
+    __version__ = "0.0.0"
+    logger.warning(f"Unable to determine package version!\nMessage: {err}")
