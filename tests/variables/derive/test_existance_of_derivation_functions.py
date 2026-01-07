6c18cdf2756f68406086351382abfc8c61330bb1
kss2k <slupphaugkjell@gmail.com>
2026-01-07 09:21:32 +0100

    Add test for existance of derivation functions

---
+++ tests/variables/derive/test_existance_of_derivation_functions.py
@@ -0,0 +1,25 @@
+from nudb_config import settings
+from nudb_config.nudb_logger import logger
+
+from nudb_use.variables.derive import __all__ as derivation_functions
+
+
+class MissingDerivationFunctions(Exception): ...
+
+
+def test_existence_of_derivation_functions():
+    confvars = settings.variables.keys()
+    missing = []
+
+    for confvar in confvars:
+        has_func = confvar in derivation_functions
+        derived_from = settings.variables[confvar].derived_from
+
+        if derived_from and not has_func:
+            missing.append(confvar)
+
+    if missing:
+        n = len(missing)
+        message = f"{n} derived variables in the config are missing derivation functions!\n{confvars}"
+        logger.error(message)
+        raise MissingDerivationFunctions(message)
