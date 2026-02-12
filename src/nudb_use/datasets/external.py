6b421fe71648b713bd855e34e9a28a1d74f3c3ae
Carl F. Corneil <aecorn@gmail.com>
2026-02-12 11:54:26 +0100

    fixing partial

--- src/nudb_use/datasets/external.py
+++ src/nudb_use/datasets/external.py
@@ -16,7 +16,7 @@ EXTERNAL_DATASETS = [
 
 
 def _generate_view(
-    dataset_name: str, alias: str, connection: db.DuckDBPyConnection
+    dataset_name: str, connection: db.DuckDBPyConnection
 ) -> None:
     path = latest_shared_paths(dataset_name)
     alias = _default_alias_from_name(dataset_name)
@@ -35,5 +35,5 @@ def _generate_view(
 
 for dataset_name in EXTERNAL_DATASETS:
     function_name = f"_generate_{dataset_name}_view"
-    globals()[function_name] = partial(dataset_name=_generate_view, dataset_name=dataset_name)
+    globals()[function_name] = partial(_generate_view, dataset_name=dataset_name)
     __all__.append(function_name)
