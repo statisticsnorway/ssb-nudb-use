d188bc89cd595d1568e8490c2945a661c46fb4f9
kss2k <slupphaugkjell@gmail.com>
2026-04-29 16:27:20 +0200

    Explicitly export generator

--- src/nudb_use/datasets/microdata_variables/__init__.py
+++ src/nudb_use/datasets/microdata_variables/__init__.py
@@ -3,3 +3,5 @@
 from nudb_use.datasets.microdata_variables.utd_hoeyeste_nus2000 import (
     _generate_microdata_utd_hoeyeste_nus2000_view,
 )
+
+__all__ = ["_generate_microdata_utd_hoeyeste_nus2000_view"]
